7a490e6344be1f963f96749ec798833545dc48be
ntzyr <vladislavsurikov@gmail.com>
2019-08-04 19:01:36 +0300

    api extension for base url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
+export const BASE_URL = 'http://jeans.ntzyr.xyz/'
+export const API_URL = BASE_URL + 'api/'
+export const STORAGE_URL = BASE_URL + 'storage/'
+
 const API = axios.create({
-  baseURL: 'http://jeans.ntzyr.xyz/api/'
+  baseURL: API_URL
 })
 
 export default API
