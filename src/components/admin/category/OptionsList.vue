bb04781cafc65c9e8b748b6844071a3181ffb281
ntzyr <vladislavsurikov@gmail.com>
2019-08-09 07:52:30 +0300

    Separate components
    Separate components for catalog relation modules

---
+++ src/components/admin/category/OptionsList.vue
@@ -0,0 +1,38 @@
+<template>
+    <q-list separator>
+        <component :is="itemType" group="categories" v-for="category in localCategories" :label="category.label" :key="category.id">
+            <q-option-group
+                    :options="category.children"
+                    v-model="localValue"
+                    :type="groupType"
+            />
+        </component>
+    </q-list>
+</template>
+
+<script>
+export default {
+  name: 'OptionsList',
+  props: {
+    groupType: String,
+    itemType: String,
+    categories: Array,
+    value: Array
+  },
+  data () {
+    return {
+      localCategories: this.categories,
+      localValue: this.value
+    }
+  },
+  watch: {
+    localValue (value) {
+      this.$emit('changeValue', value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
